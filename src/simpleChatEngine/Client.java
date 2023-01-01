a165cb493d39b29b0ede7a0961167c1dd24c1cfc
Naeem444 <naeemr.4440@gmail.com>
2023-01-01 09:39:50 +0600

    Multiple user can chat with each other on a port

--- src/simpleChatEngine/Client.java
+++ src/simpleChatEngine/Client.java
@@ -4,23 +4,49 @@ import java.io.*;
 import java.net.*;
 
 
+
+
 public class Client {
     private String host;
     private int port;
+    Socket clientSocket;
+    //gets the input stream from the socket
+    private BufferedReader input;
+    // to write I will use it and push it to server for output
+    private PrintWriter output;
     
     Client(String host, int port){
         this.host = host;
         this.port = port;
         
     }
+
+    public Socket getClientSocket() {
+        return clientSocket;
+    }
+    
     
     public void invoke(){
         try{
 //            socket: one endpoint(ip address, port) of a two-way communication link between two programs 
-//            running on the network.
+//            running on the network. (localhost ---> 127.0. 0.1)
 
-            Socket socket = new Socket(host, port);
+            clientSocket = new Socket(host, port);
             System.out.println("You are connected to the chat server. Have fun.");
+            
+            output = new PrintWriter(clientSocket.getOutputStream(), true);
+            input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            
+            HandleClient data = new HandleClient(clientSocket, output);
+            Thread clientThread = new Thread(data);
+            clientThread.start();
+            
+             String msg;
+            while ((msg = input.readLine()) != null){
+                System.out.println(msg);
+            }
+//        
+            
             
             
         }
@@ -32,18 +58,11 @@ public class Client {
         }
     }
 
-    
-    
-    
-    
-    
-    
-    
-    
+
     
     public static void main(String[] args) {
         if (args.length < 2) {
-            System.out.println("Write in cmd -> java Client.java [ip] [port]");
+            System.out.println("Write in cmd -> java Client.java [host] [port]");
             System.exit(0);
         }
         
@@ -55,6 +74,52 @@ public class Client {
         //invoking the client
         clientSocket.invoke();
     
+    }
+    
 }
+
+class HandleClient implements Runnable{
+    Socket client;
+    private PrintWriter output;
+    
+    HandleClient(Socket client , PrintWriter output){
+        this.client = client;
+        this.output = output;
+    }
+
+    @Override
+    public void run() {
+     
+        try {
+            OutputStream out = client.getOutputStream();
+            output = new PrintWriter(out, true);
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
+            while(true){
+                String msg;
+                try {
+                    
+//                        output.println("Enter Your text here: ");
+                        msg = inputReader.readLine();
+                        if(msg.startsWith("?leave")){
+                        // functionality
+                        }
+                        else{
+                            output.println(msg);
+                        }
+                    
+                    
+                } catch (IOException ex) {
+                    //handle it later
+                }
+                
+            }
+        } catch (IOException ex) {
+            //handle it later
+        } 
+    
+    }
+        
+    
+    
     
 }
--- src/simpleChatEngine/Server.java
+++ src/simpleChatEngine/Server.java
@@ -7,51 +7,135 @@ import java.util.*;
 public class Server {
     
     private int port;
-    
+    private ArrayList<HandleUser> userData = new ArrayList<>();
+
+    Server(){
+        
+    }
     Server(int port){
         this.port = port;
+        
     }
     
     //methods
     public void invoke(){
         try(ServerSocket server = new ServerSocket(port)){
-            System.out.println("Connection is now active at port: " + port);
+            System.out.println("\nConnection is now active at port: " + port + "\n\n");
             
             //It will keep accepting all the sockets that wants to get connected
             while(true){
-                Socket socket = server.accept();
-                System.out.println("A New user is connected with the server");
+                Socket clientSocket = server.accept();
+        
+                HandleUser user = new HandleUser(clientSocket, this); 
+                userData.add(user);
+                user.start(); //activating the thread
+                         
             }
-            
-            
-            
+  
             
         }
         catch(IOException e){
             System.out.println("Error occured in the server: " + e.getMessage());
         }
     }
-    
-    
- 
-    
-    
- public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.println("Write in cmd -> java Server.java [port]");
-            System.exit(0);
+    public void sendMsgtoAll(String msg){
+        // looping for every userDetail in the userData List and then send 'em all
+        for(HandleUser user: userData){
+            if(user != null){
+                user.sendMsg(msg);
+            }
         }
-        
-        int port = Integer.parseInt(args[0]);
-        Server server = new Server(port);
-        
-        
-        //invoking the server
-        server.invoke();
-        
-        
-        
+    }
+    
+
+    
+    public static void main(String[] args) {
+           // command line input verification
+           if (args.length < 1) {
+               System.out.println("Write in cmd -> java Server.java [port]");
+               System.exit(0);
+           }
+
+           int port = Integer.parseInt(args[0]);
+           Server server = new Server(port);
+
+           //invoking the server
+           server.invoke();
+
+       }
+ 
+  
+}
+
+
+class HandleUser extends Thread{
+    private Socket clientSocket;
+ 
+    PrintWriter writer;
+
+    private String name;
+    private Server server;
+    HandleUser(){
         
     }
-      
+    
+    HandleUser(Socket clientSocket, Server server){
+        this.clientSocket = clientSocket;
+        this.server = server;
+
+    }
+    
+
+    
+    @Override
+    public void run() {
+        
+        try{
+
+//           output = new PrintWriter(clientSocket.getOutputStream(), true);
+//            input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+//             System.out.println("working this");
+             
+            InputStream input = clientSocket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+ 
+            OutputStream output = clientSocket.getOutputStream();
+            writer = new PrintWriter(output, true);
+ 
+          
+            writer.println("Enter Your Name: ");
+            name = reader.readLine();
+            System.out.println(name + " is now connected with the server...\n");
+//            server.addUserName(userName);
+ 
+            String joinMsg = name + " just joined in this chat room";
+            server.sendMsgtoAll(joinMsg);
+
+            
+            String msg;
+            while ((msg = reader.readLine()) != null){
+                if(msg.startsWith("?leave")){
+                    // functionality will be added later
+                }
+                else{
+                    //sending msg to all the clients
+                    server.sendMsgtoAll( "\n"+ name + ": " + msg);
+                }
+            }
+                 
+        
+        }
+        catch(IOException e){
+            // TODO: handle it later
+        }
+        
+    }
+ 
+
+    public void sendMsg(String msg){
+            writer.println(msg);
+    }
+    
+    
 }
+
